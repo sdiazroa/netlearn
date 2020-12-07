da181ee4b09286e50bbb1a1dee2e68bc96d0999e
edwardkusell <edkzig@gmail.com>
2020-12-07 00:41:18 +0000

    test

--- application.py
+++ application.py
@@ -9,7 +9,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from helpers import apology, login_required # lookup, usd
 
-#lindsay test
+# ed's test is better than lindsay's test
 # Configure application
 app = Flask(__name__)
 
