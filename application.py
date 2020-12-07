05a0d83cb9add9d8440e0917890f9f8aca5071ac
Your Name <bader.lindsay@gmail.com>
2020-12-07 00:54:45 +0000

    resolved c

--- application.py
+++ application.py
@@ -10,8 +10,6 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required # lookup, usd
 
 
-#lindsay test #new comment
-
 # Configure application
 app = Flask(__name__)
 
